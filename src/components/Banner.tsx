a7042cf42c251f1776e352b2110d5e4699614ca5
FARVACQUE-L <laurent.fvq@gmail.com>
2026-03-29 22:23:39 +0200

    banner pics

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,23 @@
-import banner from "../assets/ban-harry-potter.png";
+import bannerTitle from "../assets/ban-harry-potter.png";
+import bannerHouse from "../assets/house.png";
+import bannerVif from "../assets/vifOr.png";
 
 import "./Banner.css";
 
 function Banner() {
 	return (
 		<section className="banner-Global">
-			<img src={banner} alt="Le titre Harry Potter" className="banner-image" />
+			<img
+				src={bannerHouse}
+				alt="Le blason des maisons"
+				className="banner-house"
+			/>
+			<img
+				src={bannerTitle}
+				alt="Le titre Harry Potter"
+				className="banner-title"
+			/>
+			<img src={bannerVif} alt="Vif d'or" className="banner-vif" />
 		</section>
 	);
 }
